a569f4a5e240861b54483be8e8162c4658e61810
Harry <harry.lees@gmail.com>
2020-02-27 21:53:02 +0000

    Add files via upload

---
+++ plantDashboardClasses.py
@@ -0,0 +1,23 @@
+import psycopg2
+
+class connection: # holds all methods for importing and exporting data from the local database
+
+        def __init__(self, username, password, host, database): # initiator runs when the class is constructed
+                try: # tries to run the following block of code
+                        self.connection = psycopg2.connect( # defines opens a psycopg2 connection to the database with the given credentials
+                                user = username,
+                                password = password,
+                                host = host,
+                                database = database,
+                        )
+
+                        self.cursor = self.connection.cursor() # initiates psycopg2 connection
+
+                except psycopg2.OperationalError as exception:
+                        print(f'[ERROR]: Unable to connect - {exception}')
+
+        def __del__(self):
+                try:
+                        self.cursor.close() # closes psycopg2 database connection.
+                except psycopg2.OperationalError as exception:
+                        print(f'[ERROR]: {exception}')
