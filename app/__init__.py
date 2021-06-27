472e778a7c0de91cfd54e4970501e463a818be98
Harry-Lees <harry.lees@gmail.com>
2021-06-27 21:51:06 +0100

    fixed database registering with app context

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 
+from .extensions import database
+
 def create_app():
     app = Flask(__name__)
     app.config.from_object('config.Config')
@@ -7,5 +9,10 @@ def create_app():
     from .core.routes import blueprint as core_blueprint
 
     app.register_blueprint(core_blueprint)
+
+    database.init_app(app)
+
+    with app.app_context():
+        database.create_all()
 
     return app
