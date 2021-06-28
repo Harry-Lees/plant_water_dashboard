f4f88966fa8d8ae1a633d13f520945300ace2b89
Harry <harry.lees@gmail.com>
2021-06-28 22:27:22 +0100

    refactored

--- read_signal.py
+++ read_signal.py
@@ -1,7 +1,10 @@
-import explorerhat
 import sqlite3
 import time
+
+import explorerhat
+
 from config import DATABASE_LOGIN
+
 
 def read_water():
     TEMPLATE = '''INSERT INTO schedule(date, voltage) VALUES(datetime('now'), ?)'''
@@ -13,6 +16,7 @@ def read_water():
 
     return voltage
 
+
 def water_plant(water_time : int = 10):
     TEMPLATE = '''INSERT INTO watered_schedule VALUES(datetime('now'))'''
 
@@ -20,13 +24,14 @@ def water_plant(water_time : int = 10):
     explorerhat.light[0].on()
     
     time.sleep(water_time)
-    
+
     explorerhat.output[0].off()
     explorerhat.light[0].off()
 
     with sqlite3.connect(DATABASE_LOGIN) as connection:
         cursor = connection.cursor()
         cursor.execute(TEMPLATE)
+
 
 if __name__ == '__main__':
     voltage = read_water()
