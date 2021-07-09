88ee540bbf981ca5875f7f05d968eb8f14fc1c77
Harry-Lees <harry.lees@gmail.com>
2021-07-09 01:29:40 +0100

    added plants_tab to track custom plants

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from .extensions import database
 
+
 class Watered(database.Model):
     __tablename__: str = 'watered_tab'
 
@@ -11,13 +12,23 @@ class Schedule(database.Model):
 
     id: database.Column = database.Column(database.Integer, primary_key=True)
 
-    plant_id: database.Column = database.Column(database.Integer)
-    datetime: database.Column = database.Column(database.DateTime, nullable=False)
+    plant_id: database.Column = database.Column(database.Integer, database.ForeignKey('plants_tab.id'), nullable=False)
 
-    # sensor statistics
-    voltage: database.Column = database.Column(database.Float, nullable=True)
-    temp: database.Column = database.Column(database.Float, nullable=True)
-    humidity: database.Column = database.Column(database.Float, nullable=True)
+    datetime: database.Column = database.Column(database.DateTime, nullable=False)
+    water_level: database.Column = database.Column(database.Float, nullable=False)
 
     def __repr__(self) -> str:
         return f'Schedule(plant={self.plant_id}, datetime={self.datetime})'
+
+
+class Plant(database.Model):
+    __tablename__: str = 'plants_tab'
+
+    id: database.Column = database.Column(database.Integer, primary_key=True)
+    schedule = database.relationship('Schedule', backref='plants_tab', lazy=True)
+
+    room: database.Column = database.Column(database.String, nullable=False)
+    name: database.Column = database.Column(database.String, nullable=False)
+
+    def __repr__(self) -> str:
+        return f'Plant(id={self.id}, name={self.name}, room={self.room})'
