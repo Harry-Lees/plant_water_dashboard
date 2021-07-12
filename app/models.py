fb8e3d0e39548de912ebf56330afc039d8e7a1ff
Harry-Lees <harry.lees@gmail.com>
2021-07-12 10:48:15 +0100

    added ForeignKey for watered_tab and added some comments to classes

--- app/models.py
+++ app/models.py
@@ -2,12 +2,22 @@ from .extensions import database
 
 
 class Watered(database.Model):
+    '''Model to hold the data for when the plant was last watered.'''
+
     __tablename__: str = 'watered_tab'
 
-    date_watered: database.Column = database.Column(database.DateTime, primary_key=True)
+    id: database.Column = database.Column(database.Integer, primary_key=True)
+
+    date_watered: database.Column = database.Column(database.DateTime, nullable=False)
+    plant_id: database.Column = database.Column(database.Integer, database.ForeignKey('plants_tab.id'), primary_key=True)
+
+    def __repr__(self) -> str:
+        return f'Watered(plant={self.plant_id}, date_watered={self.date_watered})'
 
 
 class Schedule(database.Model):
+    '''Model to hold the values collected from the moisture sensors for a given plant.'''
+
     __tablename__: str = 'schedule_tab'
 
     id: database.Column = database.Column(database.Integer, primary_key=True)
@@ -22,10 +32,15 @@ class Schedule(database.Model):
 
 
 class Plant(database.Model):
+    '''Model to represent a plant.'''
+
     __tablename__: str = 'plants_tab'
 
     id: database.Column = database.Column(database.Integer, primary_key=True)
+
+    # Relationships
     schedule = database.relationship('Schedule', backref='plants_tab', lazy=True)
+    watered = database.relationship('Watered', backref='watered_tab', lazy=True)
 
     room: database.Column = database.Column(database.String, nullable=False)
     name: database.Column = database.Column(database.String, nullable=False)
