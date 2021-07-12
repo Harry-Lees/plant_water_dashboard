dbd0ab6d2e8a91a566e84c3f3a4b5c6679739a0e
Harry-Lees <harry.lees@gmail.com>
2021-07-12 10:49:05 +0100

    added support for multiple plants and added framework for adding more custom plants

--- app/core/routes.py
+++ app/core/routes.py
@@ -1,8 +1,8 @@
 from datetime import datetime, timedelta
-from typing import List, Optional, Dict, Any
+from typing import List, Optional, Dict, Any, Tuple
 from dataclasses import dataclass
 
-from flask import Blueprint, render_template, current_app
+from flask import Blueprint, render_template, current_app, request
 from scipy import stats
 
 from app.models import Watered, Schedule
@@ -11,15 +11,11 @@ from app.models import Watered, Schedule
 blueprint = Blueprint('core', __name__, template_folder='templates')
 
 
-@dataclass
-class Plant:
-    title: str
-    location: str
+def next_water_due(plant_id: int) -> str:
+    '''function to get the next water due for the given plant.'''
 
-
-def next_water_due() -> str:
     filter = Schedule.datetime.between(datetime.now() - timedelta(days = 10), datetime.now())
-    water_data: List[Schedule] = Schedule.query.filter(filter).all()
+    water_data: List[Schedule] = Schedule.query.filter_by(plant_id=plant_id).filter(filter).all()
 
     if water_data:
         x, y = zip(*[(w.datetime.timestamp(), w.voltage) for w in water_data])
@@ -33,14 +29,19 @@ def next_water_due() -> str:
     return 'n/a'
 
 
-def last_time_watered() -> Optional[Watered]:
-    watered = Watered.query.order_by('date_watered').first()
+def last_time_watered(plant_id: int) -> Optional[Watered]:
+    '''function to get the last time the plant was watered'''
+
+    watered = Watered.query.filter_by(plant_id=plant_id).order_by('date_watered').first()
     if watered:
         return watered.date_watered
+    else:
+        return None
 
 
-@blueprint.route('/graph_data')
-def graph_logic():
+def graph_logic(plant_id: int) -> Tuple[list, list]:
+    '''function to return the Graph data for the watered graph on the index page.'''
+
     x = []
     y = []
     graph_length = current_app.config['GRAPH_LENGTH']
@@ -50,7 +51,7 @@ def graph_logic():
         date = start_date + timedelta(days=i)
         x.append(date.strftime('%Y-%m-%d'))
 
-        schedule = Schedule.query.filter(Schedule.datetime == date).first()
+        schedule = Schedule.query.filter_by(datetime=date, plant_id=plant_id).first()
         if schedule:
             y.append(schedule.voltage)
         else:
@@ -58,30 +59,20 @@ def graph_logic():
     return x, y
 
 
-@blueprint.route('/')
+@blueprint.route('/', methods=['GET', 'POST'])
 def index():
-    plants: List[Plant] = [
-        Plant('Pothos', 'Living Room'),
-    ]
+    '''function to serve the index page for the user'''
 
-    x, y = graph_logic()
+    selected_plant: str = request.args.get('plant', default=0, type=int)
+    x, y = graph_logic(selected_plant)
 
     args: Dict[str, Any] = {
-        'plants'            : plants,
-        'last_time_watered' : last_time_watered(),
-        'next_water_due'    : next_water_due(),
+        'plants'            : None,
+        'last_time_watered' : last_time_watered(selected_plant),
+        'next_water_due'    : next_water_due(selected_plant),
         'x_axis_dates'      : x,
         'graph_data'        : y,
+        'selected'          : selected_plant,
     }
 
     return render_template('index.html', **args)
-
-
-@blueprint.route('/add_plant')
-def add_plant():
-    pass
-
-
-@blueprint.route('/remove_plant')
-def remove_plant():
-    pass
